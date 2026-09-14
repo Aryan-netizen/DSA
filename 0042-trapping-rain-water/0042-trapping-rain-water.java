85bd98b3ee62a96d40305d3f808db73dde046218
Aryan Garg <gargaryan12122005@gmail.com>
2026-09-14 14:58:26 +0530

    Time: 0 ms (100%), Space: 47.6 MB (66.33%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,32 +1,26 @@
 class Solution {
-    public int[] left(int[] arr,int n){
-        int[] l=new int[n];
-        int max=arr[0];
-        l[0]=max;
-        for(int i=1;i<n;i++){
-            max=Math.max(arr[i],max);
-            l[i]=max;
-        }
-        return l;
-    }
-    public int[] right(int[] arr,int n){
-        int[] r=new int[n];
-        int max=arr[n-1];
-        r[n-1]=max;
-        for(int i=n-1;i>=0;i--){
-            max=Math.max(arr[i],max);
-            r[i]=max;
-        }
-        return r;
-    }
+    
     public int trap(int[] arr) {
-        int n=arr.length;
-        int[] leftmax = left(arr,n);
-        int[] rightmax = right(arr,n);
+        int lMax =0;
+        int rMax =0;
+        int i=0,j=arr.length-1;
         int area=0;
-        for(int i=1;i<n-1;i++){
-            int ans=Math.min(leftmax[i],rightmax[i])-arr[i];
-            area+=ans;
+        while(i<=j){
+            if(arr[i]<=arr[j]){
+                if(arr[i]<lMax){
+                    area+=lMax-arr[i];
+                }else{
+                    lMax=arr[i];
+                }
+                i++;
+            }else{
+                if(arr[j]<rMax){
+                    area+=rMax-arr[j];
+                }else{
+                    rMax=arr[j];
+                }
+                j--;
+            }
         }
         return area;
     }
